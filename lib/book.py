19ece77db456f52604b40f5a820a5276bde931ef
victor <vktubei@gmail.com>
2025-04-14 12:21:28 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title="Unknown",page_count=0):
+        self.title = title
+        self.page_count = page_count
+    def set_page_count(self, value):
+        if isinstance(value, int):
+            self.page_count = value
+        else:
+            print("page_count must be an integer\n")
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
